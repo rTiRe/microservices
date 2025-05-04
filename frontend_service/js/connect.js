6db2dfbc2ede663651888c99e41bec99896aa081
TiRe <zientenin@mail.ru>
2025-05-05 00:41:46 +0300

    Initial

---
+++ frontend_service/js/connect.js
@@ -0,0 +1,166 @@
+let username = "";
+let password = "";
+
+let refresh_token = "";
+let connected = false;
+
+const headers = new Headers();
+headers.append("Content-Type", "application/x-www-form-urlencoded");
+
+const getToken = async () => {
+    console.log("get token called");
+    const params = new URLSearchParams();
+    params.append("client_id", "myclient");
+    const requestOptions = {
+        method: "POST",
+        headers: headers,
+        body: params,
+    };
+    if (refresh_token == "") {
+        params.append("username", username);
+        params.append("password", password);
+        params.append("grant_type", "password");
+    } else {
+        params.append("refresh_token", refresh_token);
+        params.append("grant_type", "refresh_token");
+    }
+    return fetch(
+        "http://127.0.0.1:7070/realms/myrealm/protocol/openid-connect/token",
+        requestOptions
+    )
+        .then((response) => response.json())
+        .then((result) => {
+            console.log("token accepted");
+            refresh_token = result.refresh_token;
+            return result.access_token;
+        })
+        .catch((error) => console.error(error));
+};
+
+const client = new Centrifuge(
+    "ws://127.0.0.1:8080/centrifugo/connection/websocket",
+    {
+        getToken: getToken,
+        debug: true,
+        resubscribe: true,
+    }
+);
+
+client.on("connected", () => {
+    password = null;
+    document.getElementById("password").value = "";
+    connected = true;
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("userInfo").style.display = "flex";
+});
+
+client.on("disconnected", () => {
+    refresh_token = "";
+    client.setToken("");
+    connected = false;
+    document.getElementById("loginForm").style.display = "block";
+    document.getElementById("userInfo").style.display = "none";
+    document.getElementById("userHello").textContent = "Hello, " + username;
+});
+
+document.getElementById("btnLogin").addEventListener("click", () => {
+    if (connected) {
+        return;
+    }
+    username = document.getElementById("username").value;
+    password = document.getElementById("password").value;
+    if (username != "" && password != "") {
+        client.connect();
+    } else {
+        console.error("empty username or password");
+    }
+});
+
+document.getElementById("btnLogout").addEventListener("click", () => {
+    if (!connected) {
+        return;
+    }
+    console.log("disconnecting");
+    const params = new URLSearchParams();
+    params.append("client_id", "myclient");
+    const requestOptions = {
+        method: "POST",
+        headers: headers,
+        body: params,
+    };
+    if (refresh_token != "") {
+        params.append("refresh_token", refresh_token);
+        params.append("grant_type", "refresh_token");
+        fetch(
+            "http://127.0.0.1:7070/realms/myrealm/protocol/openid-connect/logout",
+            requestOptions
+        )
+            .then((response) => {
+                console.log("session closed");
+            })
+            .catch((error) => console.error(error))
+            .finally(() => {
+                client.disconnect();
+            });
+    } else {
+        console.error("no refresh token");
+    }
+});
+
+const channelInput = document.getElementById("channel");
+const messageInput = document.getElementById("message");
+const messages = document.getElementById("messages");
+let current_sub = undefined;
+let current_channel = undefined;
+
+document.getElementById("btnConnect").addEventListener("click", () => {
+    const channel_name = channelInput.value;
+    const sub = client.newSubscription(channel_name);
+    sub.on("publication", (msg) => {
+        let line = `<p><strong>${msg.data.from}:</strong> ${msg.data.message}</p>`;
+        messages.innerHTML += line;
+    })
+    let channel = document.createElement("li");
+    let disconnect_button = document.createElement("button");
+    let watch_button = document.createElement("button");
+    disconnect_button.addEventListener("click", () => {
+        client.removeSubscription(sub);
+        channel.remove();
+    });
+    disconnect_button.innerHTML = "Disconnect";
+    watch_button.style.marginLeft = "5px";
+    watch_button.addEventListener("click", () => {
+        if (current_sub != sub) {
+            if (current_sub !== undefined) {
+                current_sub.unsubscribe();
+            }
+            sub.subscribe();
+            current_sub = sub;
+            messages.innerHTML = "";
+            document.getElementById("chat").style.display = "flex";
+            if (current_channel !== undefined) {
+                current_channel.style.fontWeight = "normal";
+                current_channel.style.fontSize = "initial";
+            }
+            channel.style.fontWeight = "bold";
+            channel.style.fontSize = "18px";
+            current_channel = channel;
+        }
+    });
+    watch_button.innerHTML = "Watch";
+    channel.innerHTML = channel_name + " ";
+    channel.appendChild(disconnect_button);
+    channel.appendChild(watch_button);
+    document.getElementById("subscriptions").appendChild(channel);
+});
+
+
+document.getElementById("btnPublish").addEventListener("click", () => {
+    if (messageInput.value != "") {
+        current_sub.publish({
+            from: username,
+            message: messageInput.value
+        })
+        messageInput.value = "";
+    }
+});
