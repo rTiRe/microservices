10fa10fa83a86ca0351ad22221bc5e0396c1c37c
TiRe <zientenin@mail.ru>
2025-05-05 15:34:45 +0300

    @

--- backend_service/src/api/grpc/services/proxy.py
+++ backend_service/src/api/grpc/services/proxy.py
@@ -5,6 +5,8 @@ import grpc
 import jwt
 
 from src.api.grpc.protobufs import proxy_pb2_grpc, proxy_pb2
+from src.api.sqlc import ws_requests
+from src.storage.postgres import engine
 
 
 PUBLIC_KEY = '-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEApWUuKoo/aKrDUZxiWLQT7jl64cCLg2ibTrWRHcJdUakA/YEcSkqGs4vX3iWNcCwfjRkBgMes2/aCEC1LULX63OaZrsn7SmzT1jLrmynR6DN0GU4iYsGTYIG6EqxnRcia8G5xkY6adFmGEtL7tiwRKc2VFZOAT02o9ehu49yzHfM6aHkv1ioWOzm6EgrRKUssOTGGUaDVTsETlbF2/ZcvaMnJ9BLszBkdTcOPvQRKEldzWGR1ztmrqspHSZlyLWWEzZbOwDK8nYZjWyte6oLzQK092T6LtBTdpLR6u0AJclPGmycRg6nr645+LYKyuZweYZR94U3IJ8ZPsQx9aBed/QIDAQAB\n-----END PUBLIC KEY-----'
@@ -29,10 +31,22 @@ class ProxyService(proxy_pb2_grpc.CentrifugoProxyServicer):
         user_data = {
             'id': token_data['sub'],
             'username': token_data['preferred_username'],
-            'first_name': token_data['given_name'],
-            'last_name': token_data['family_name'],
+            'given_name': token_data['given_name'],
+            'family_name': token_data['family_name'],
             'email': token_data['email'],
         }
+        async with engine.connect() as connection:
+            querier = ws_requests.AsyncQuerier(connection)
+            user = await querier.get_user_by_id(id=user_data['id'])
+            if not user:
+                await querier.create_user(
+                    id=user_data['id'],
+                    username=user_data['username'],
+                    given_name=user_data['given_name'],
+                    family_name=user_data['family_name'],
+                    enabled=True,
+                )
+            await connection.commit()
         return proxy_pb2.ConnectResponse(
             result=proxy_pb2.ConnectResult(
                 user=user_data['id'],
